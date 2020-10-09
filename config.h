00b5f6bc850bc42c64378cfebbbf7e7afdfccbc6
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-10-09 18:47:30 +0100

    General code organisation

--- config.h
+++ config.h
@@ -142,7 +142,7 @@ static unsigned int defaultrcs = 256;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 6;
+static unsigned int cursorshape = 4;
 
 /*
  * Default columns and rows numbers
