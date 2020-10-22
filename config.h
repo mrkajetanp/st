e0abcaadb3e2fb9a41ed134662adc8e157a3c01a
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-10-22 23:36:49 +0100

    Switched to Monaco font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font:size=10";
+static char *font = "MonacoBSemi:size=10";
 static int borderpx = 12;
 
 /*
@@ -26,8 +26,8 @@ char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
-static float cwscale = 1.0;
-static float chscale = 1.0;
+static float cwscale = 0.8;
+static float chscale = 0.9;
 
 /*
  * word delimiter string
